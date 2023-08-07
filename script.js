fb751a88895e024a723092a6cbb344ca12f3775b
mpetkov228 <mpetkov228@gmail.com>
2023-08-07 16:16:52 +0300

    Add Book constructor

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
